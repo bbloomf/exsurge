59d990c5417c857faddf3b28a6f8413e8e327165
Benjamin Bloomfield <bhb123@gmail.com>
2023-06-20 14:15:15 -0400

    Fix type definitions

--- src/index.d.ts
+++ src/index.d.ts
@@ -127,7 +127,7 @@ declare module "exsurge" {
     pages: ChantScore[];
     updateSelection(selection: Selection);
     createSvgTree(ctxt: ChantContext, zoom?: number): SvgTreeNode;
-    createSvgFragment(ctxt: ChantContext): string;
+    createSvg(ctxt: ChantContext): string;
     recreateDropCap(ctxt: ChantContext): void;
     updateNotations(ctxt: ChantContext): void;
     performLayout(ctxt: ChantContext, forceLayout: boolean): void;
